2d79b8a5b4fd50cc1f1989240d2eb5eb7849ef35
Kayla Reap <kaylareap@Kaylas-MBP.centurylink.net>
2019-10-04 18:43:32 -0500

    Finished

--- Colors/ContentView.swift
+++ Colors/ContentView.swift
@@ -6,16 +6,50 @@
 //  Copyright © 2019 Kayla Reap. All rights reserved.
 //
 
-import SwiftUI
+import UIKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+class ChecklistViewController: UITableViewController {
+    @IBOutlet weak var sampleTableView: UITableView
+
+    var rowTimes = ["red", "orange", "yellow", "green","blue", "purple", "brown"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView, numberOfRows section: Int) -> Int {
+        return rowItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        cell7.textLabel7.text = rowItems[indexPath.row]
+        return cell
+    }
+        
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, willDisplay cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if(indexPath.row % 2 == 0) {
+            cell.backgroundColor = UIColor.redColor()
+        }
+        else {
+            cell.backgroundColor = UIColor.orangeColor()
+        }
+        else {
+            cell.backgroundColor = UIColor.yellow
+        }
+        else {
+            cell.backgroundColor = UIColor.green
+        }
+        else {
+            cell.backgroundColor = UIColor.purple
+        }
+        else {
+            cell.backgroundColor = UIColor.brown
+        }
     }
 }
